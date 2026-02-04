32ab80577c287c7cc04303776ad5880a0579c2c3
EwaME <edwardmaradiagaespinal@gmail.com>
2026-02-03 19:48:21 -0600

    API BUENAAAA

--- src/Servicios/api.js
+++ src/Servicios/api.js
@@ -1,11 +1,15 @@
+const BASE_URL = process.env.REACT_APP_API_URL || "https://backendprueba-hwcs.onrender.com";
+
+const API_PATH = `${BASE_URL}/api/servicios`;
+
 export async function getServicios() {
-    const response = await fetch("https://backendprueba-hwcs.onrender.com");
+    const response = await fetch(API_PATH);
     if (!response.ok) throw new Error("Error al cargar servicios");
     return response.json();
 }
 
 export async function crearServicio(payload) {
-    const res = await fetch("https://backendprueba-hwcs.onrender.com", {
+    const res = await fetch(API_PATH, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
@@ -19,10 +23,8 @@ export async function crearServicio(payload) {
     return res.json();
 }
 
-const BASE_URL = "https://backendprueba-hwcs.onrender.com";
-
 export async function actualizarServicio(id, payload) {
-    const res = await fetch(`${BASE_URL}/${id}`, {
+    const res = await fetch(`${API_PATH}/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
